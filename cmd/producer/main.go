a1c2e1f2286ba80d0a242f3ffb2f2aff1efbf8d2
Herllon Cardoso <herllon.cardoso@govoll.com>
2022-04-04 20:13:12 -0300

    feat: producing a message to topic

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
 
 func main() {
-	fmt.Println("Hello Go")
+	producer := NewKafkaProducer()
+	Publish("mensagem", "teste", producer, nil)
+	producer.Flush(1000)
+}
+
+func NewKafkaProducer() *kafka.Producer {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "gokafka_kafka_1:9092",
+	}
+	p, err := kafka.NewProducer(configMap)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return p
+}
+
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+	message := &kafka.Message{
+		Value:          []byte(msg),
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Key:            key,
+	}
+	err := producer.Produce(message, nil)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/herlloncardoso/gokafka
 
 go 1.16
+
+require github.com/confluentinc/confluent-kafka-go v1.8.2 // indirect
